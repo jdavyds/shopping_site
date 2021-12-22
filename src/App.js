5b4cba4e65af10da3f50ccef75436d71eac98b25
jdavyds <slimjdavids@gmail.com>
2021-12-22 19:57:15 +0100

    home

--- src/App.js
+++ src/App.js
@@ -69,8 +69,8 @@ function App() {
         handleDecrease={handleDecrease}
         handleCheckOut={handleCheckOut}
          />
-      <Switch>
         <Route exact path="/" component={Home} />
+      <Switch>
         <Route exact path="/shop"
         render={() => <Shop 
           total={total} 
